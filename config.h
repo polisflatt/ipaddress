7d9fd3778ad67a31e91726f9dae6c470859179e3
polisflatt <30891943+polisflatt@users.noreply.github.com>
2018-12-20 00:13:59 -0600

    Update config.h

--- config.h
+++ config.h
@@ -8,6 +8,6 @@
 /* The minimal configuration file for ipaddr */ 
 
 /* Friendly section */
-char* IP_LINK = "ipinfo.io"; /* IP/Domain to curl, to get your IP. */
-char* DEFAULT_ADAPTER = "enp0s25"; /* Put to NULL if you want to disable this feature. */
-p_ip_type preferred_ip_type = p_ipv4;
+char* IP_LINK = ""; /* IP/Domain to curl, to get your IP. I use: icanhazmyip.com*/
+char* DEFAULT_ADAPTER = ""; /* Put to NULL if you want to disable this feature. */
+p_ip_type preferred_ip_type = p_ipv4; (p_ipv4, p_ipv6; whatever you want to use; default is ipv4)
